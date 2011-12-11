94341e02f4c9fe6a8136f067c161d493965a6905
Yudong Tan <yudong.tan@gmail.com>
2011-12-10 21:37:49 -0800

    add scraper for hotel_base.com

--- schema/tables.sql
+++ schema/tables.sql
@@ -2,12 +2,14 @@ DROP TABLE IF EXISTS email, business;
 CREATE TABLE business (
        name     VARCHAR(40) NOT NULL,
        url      VARCHAR(40) NOT NULL,
-       phone    VARCHAR(10),
+       phone    VARCHAR(40),
+       address  VARCHAR(100),
        category ENUM('AGY', 'RLS', 'EDU', 'TCH', 'TUR') NOT NULL,
        country  VARCHAR(10) NOT NULL,
        status   ENUM('U', 'P', 'A', 'C') NOT NULL,
        owner    VARCHAR(20),
        id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+       UNIQUE KEY (url),
        PRIMARY KEY (id)
 ) ENGINE = InnoDB;
 
@@ -17,6 +19,7 @@ CREATE TABLE email (
        last_contacted DATE,
        priority       INT UNSIGNED NOT NULL,
        id             INT UNSIGNED NOT NULL AUTO_INCREMENT,
+       UNIQUE KEY (address),
        PRIMARY KEY(id),
        FOREIGN KEY (biz_id) REFERENCES business (id)
 ) ENGINE = InnoDB;
